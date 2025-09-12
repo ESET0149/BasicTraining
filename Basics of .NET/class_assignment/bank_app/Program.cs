27c8b54db4b7097f7f6a76dcfa7400eed9b6a1c5
Vinshranjan311 <vinish.ranjan@esyasoft.com>
2025-09-12 13:28:06 +0530

    adding class task of .NET

---
+++ Basics of .NET/class_assignment/bank_app/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace BankingApp
+{
+    internal class Program
+    {
+        static int CreateAccount()
+        {
+            Console.WriteLine("Enter new account number for customer:");
+            int accountNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Congratulations !! Your Account created. Your account number is : " + accountNumber);
+
+            return accountNumber;
+        }
+
+        static (int accountNumber, int TotalAmount) DepositMoney(int accountNumber)
+        {
+            int TotalAmount;
+            Console.WriteLine("Enter Deposit Amount");
+
+            int InputAmount = Convert.ToInt32(Console.ReadLine());
+            int AccountNum = accountNumber;
+            TotalAmount = TotalAmount + InputAmount;
+            Console.WriteLine("Amount:" + InputAmount + "deposited to Account Number:" + AccountNum);
+
+
+            return TotalAmount;
+
+        }
+        static void Main(string[] args)
+        {
+            int TotalAmount, AccountNumber;
+            while (true)
+            {
+                Console.WriteLine("1:Create New Account");
+                Console.WriteLine("2:Deposit Money");
+                Console.WriteLine("3:Withdraw Money");
+                Console.WriteLine("4:Display Account Detail");
+                Console.WriteLine("5:Exit");
+                Console.WriteLine("\n Enter your choice");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    AccountNumber = CreateAccount();
+                }
+
+                if (choice == 2)
+                {
+                    TotalAmount = DepositMoney(767);
+                    Console.WriteLine("Total Amount:" + TotalAmount + "deposited to Account Number:" + AccountNumber);
+                }
+            }
+
+
+
+
+        }
+    }
+}
